c61d4b80e899e1bacac8da365d0164cecaeb3f3d
pedro_carmezim <pedro.carmezim@novabase.pt>
2019-09-28 02:06:37 +0100

    add endponit to be testes in kubernetes

--- server.js
+++ server.js
@@ -17,16 +17,28 @@ app.set('view engine', 'pug');
 app.listen(portt, () => console.log('App listening on portt ' + portt))
 
 
+var ipv4 = "";
+var info = "";
+
 app.get('/', function (req, res) {
+  
+  // Requests for specific Kubernetes
+  request('http://169.254.169.254/latest/meta-data/public-ipv4', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    ipv4 = body;
+  });
+
   res.render('indexone', 
   { 
     title: "title", 
     homedir: os.homedir(),
     hostname: os.hostname(), 
     platform: os.platform(),
-    freememory: os.freemem(), 
-    totalmemory: os.totalmem(),
+    freememory: Math.round(os.freemem()/1000000), 
+    totalmemory: Math.round(os.totalmem()/10000000),
     release: os.release(), 
+    ipv4kube: ipv4,
+
 });
 });
 
